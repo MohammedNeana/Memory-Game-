c86c8528b2fe36a4a107ef352b8c07bbce1745ca
hagerMhmd <hagermuhammedibrahem@gmail.com>
2022-11-17 15:49:13 +0200

    changes

--- Js/main.js
+++ Js/main.js
@@ -2,17 +2,17 @@ let startGame = document.querySelector('.control-buttons span'),
     name = document.querySelector('.name'),
     controlButtons = document.querySelector('.control-buttons')
 
-// startGame.addEventListener('click', _ => {
-//     let userName = prompt('Whats Your Name')
-//     userName == '' ? name.innerHTML = 'Unknown' : name.innerHTML = userName
-//     controlButtons.remove()
-// })
+startGame.addEventListener('click', _ => {
+    let userName = prompt('Whats Your Name')
+    userName == '' ? name.innerHTML = 'Unknown' : name.innerHTML = userName
+    controlButtons.remove()
+})
 const duration = 1000,
     blockContainer = document.querySelector('.memory-game-blocks'),
     blocks = Array.from(blockContainer.children),
     orderRange = [...Array(blocks.length).keys()]
 
-blocks.forEach((block, index) => {
+blocks.forEach((block) => {
     const i = Math.floor(Math.random(orderRange) * blocks.length);
-    block.style.order = orderRange[index]
+    block.style.order = orderRange[i]
 })
